5ab4d50aec0e8e308c98b36be8bcc6748020b171
Kobe Hsu <nekokobe@gmail.com>
2015-07-06 14:23:23 +0800

    107 Binary Tree Level Order Traversal II

--- src/algorithms/Problem107.java
+++ src/algorithms/Problem107.java
@@ -1,5 +1,6 @@
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,19 +11,65 @@ import java.util.List;
  */
 public class Problem107 {
 
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+  public List<List<Integer>> levelOrderBottom(TreeNode root) {
 
-        return null;
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+    List<List<TreeNode>> levelNodes = new ArrayList<List<TreeNode>>();
+    if (root == null) {
+      return result;
+    }
+
+    List<TreeNode> nodes = new ArrayList<TreeNode>();
+    nodes.add(root);
+    levelNodes.add(nodes);
+
+    while (nodes.size() > 0) {
+
+      List<TreeNode> tmpNodes = new ArrayList<TreeNode>();
+
+      for (TreeNode node : nodes) {
+        if (node.left != null) {
+          tmpNodes.add(node.left);
+        }
+        if (node.right != null) {
+          tmpNodes.add(node.right);
+        }
+      }
+
+      if (tmpNodes.size() > 0) {
+        levelNodes.add(tmpNodes);
+      }
+      nodes = tmpNodes;
 
     }
 
-    public static void main(String args[]) {
+    // reverse
+    for (int i = levelNodes.size() - 1; i >= 0; i--) {
 
-        TreeNode emptyTree = null;
-        TestUtil.traverseTree(emptyTree);
+      List<Integer> level = new ArrayList<Integer>();
+      for (TreeNode node : levelNodes.get(i)) {
+        level.add(node.val);
+      }
 
-        Problem107 p = new Problem107();
+      result.add(level);
 
     }
+
+    return result;
+
+  }
+
+
+
+  public static void main(String args[]) {
+
+    TreeNode root = TestUtil.generateCompleteTree(22);
+    // TestUtil.traverseTree(root);
+
+    Problem107 p = new Problem107();
+    p.levelOrderBottom(root);
+
+  }
 
 }
