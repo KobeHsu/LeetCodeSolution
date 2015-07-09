ad1c3e7310628a338bdf0597ae020df1d3aac1cb
Kobe Hsu <nekokobe@gmail.com>
2015-07-10 01:48:57 +0800

    58 Length of Last Word

---
+++ src/algorithms/Problem58.java
@@ -0,0 +1,50 @@
+package algorithms;
+
+/**
+ * Length of Last Word
+ *
+ * @author kobe
+ *
+ */
+public class Problem58 {
+
+  public int lengthOfLastWord(String s) {
+
+    int length = 0;
+
+    if (s==null || s.length()==0 ) {
+      return length;
+    }
+    
+    char[] charAry = s.toCharArray();
+    
+    boolean isWord = false;
+    for (int i=charAry.length-1; i>=0; i--) {
+      
+      if (charAry[i]!=' ') {
+        isWord = true;
+        length++;
+      } else if(isWord){
+        break;
+      }
+      
+    }
+
+    return length;
+
+  }
+
+  public static void main(String args[]) {
+
+    Problem58 p = new Problem58();
+
+    String s = "Hello World";
+
+    long start = System.currentTimeMillis();
+    System.out.println(p.lengthOfLastWord(s));
+    long end = System.currentTimeMillis();
+    System.out.println(end - start + "ms");
+
+  }
+
+}
