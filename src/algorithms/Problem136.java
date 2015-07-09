6d480f7abc42a92c018608e15c28ed08b7008ab3
Kobe Hsu <nekokobe@gmail.com>
2015-07-10 00:58:44 +0800

    short

--- src/algorithms/Problem136.java
+++ src/algorithms/Problem136.java
@@ -11,7 +11,7 @@ public class Problem136 {
   public int singleNumber(int[] nums) {
     int single = 0;
     for (int i : nums) {
-      single = single ^ i;
+      single ^= i;
     }
     return single;
   }
