aa2b18949ebc569c3a8b480ae2281857315e40ac
Kobe Hsu <nekokobe@gmail.com>
2015-07-16 03:28:02 +0800

    not solved

---
+++ src/algorithms/Problem223.java
@@ -0,0 +1,24 @@
+package algorithms;
+
+/**
+ * Rectangle Area
+ *
+ * @author kobe
+ *
+ */
+public class Problem223 {
+
+  public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+
+    // TODO: not solved
+    return H;
+
+  }
+
+  public static void main(String args[]) {
+
+    Problem223 p = new Problem223();
+
+  }
+
+}
