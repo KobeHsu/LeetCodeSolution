9f1673941d83e420651cd8272ee78215660f2302
Kobe Hsu <nekokobe@gmail.com>
2015-07-16 03:15:36 +0800

    a template

---
+++ src/algorithms/ProblemN.java
@@ -0,0 +1,23 @@
+package algorithms;
+
+/**
+ * Template
+ *
+ * @author kobe
+ *
+ */
+public class ProblemN {
+
+  public void foo() {}
+
+  public static void main(String args[]) {
+
+    ProblemN p = new ProblemN();
+
+    long start = System.currentTimeMillis();
+    p.foo();
+    long end = System.currentTimeMillis();
+    System.out.println(end - start + "ms");
+
+  }
+}
