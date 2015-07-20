b1f4c94befe33f304509c1332ff95ef73255ea87
Kobe Hsu <nekokobe@gmail.com>
2015-07-20 16:09:44 +0800

    27 Remove Duplicates from Sorted Array

---
+++ src/algorithms/Problem27.java
@@ -0,0 +1,47 @@
+package algorithms;
+
+/**
+ * Remove Duplicates from Sorted Array
+ *
+ * @author kobe
+ *
+ */
+public class Problem27 {
+
+  public int removeDuplicates(int[] nums) {
+
+    int len = nums.length;
+    if (len < 2) {
+      return len;
+    }
+
+    int c = 1;
+    int n = nums[0];
+    for (int i = 1; i < len; i++) {
+      if (nums[i] != n) {
+        c++;
+        n = nums[i];
+        nums[c - 1] = nums[i];
+      }
+    }
+
+    return c;
+  }
+
+  public static void main(String args[]) {
+
+    Problem27 p = new Problem27();
+
+    int[] nums = new int[] {1, 1, 2, 3, 4, 5, 5, 6, 6, 6, 7};
+
+    long start = System.currentTimeMillis();
+    System.out.println(p.removeDuplicates(nums));
+    for (int n : nums) {
+      System.out.print(n + ",");
+    }
+    System.out.println();
+    long end = System.currentTimeMillis();
+    System.out.println(end - start + "ms");
+
+  }
+}
