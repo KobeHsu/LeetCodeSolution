a069383ba2cda72496bd33ab75463bfd7763befc
Kobe Hsu <nekokobe@gmail.com>
2015-07-24 03:35:37 +0800

    ref solution

--- src/algorithms/Problem066.java
+++ src/algorithms/Problem066.java
@@ -67,6 +67,28 @@ public class Problem066 {
 
   }
 
+  public int[] plusOne_(int[] digits) {
+    
+    if (digits == null || digits.length == 0) {
+      return new int[0];
+    }
+
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      } else {
+        digits[i] = 0;
+      }
+    }
+
+    int[] result = new int[digits.length + 1];
+    result[0] = 1;
+
+    return result;
+    
+  }
+
   public static void main(String args[]) {
 
     Problem066 p = new Problem066();
